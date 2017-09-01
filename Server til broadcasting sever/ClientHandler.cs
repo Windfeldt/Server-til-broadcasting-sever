2232ec707970ed49ce123655126b0fc18305e48e
Rasmus <Rasmus@DESKTOP-4F5C1V6>
2017-09-01 08:53:08 +0200

    Add project files.

---
+++ Server til broadcasting sever/ClientHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+
+namespace Server_til_broadcasting_sever
+{
+    class ClientHandler
+    {
+        Thread clientThread;
+        TcpClient newClient;
+
+        NetworkStream nWS;
+        StreamReader sR;
+        StreamWriter sW;
+
+        public ClientHandler(TcpClient newClient)
+        {
+            this.newClient = newClient;
+            nWS = newClient.GetStream();
+            sR = new StreamReader(nWS);
+            sW = new StreamWriter(nWS);
+            StartClient();
+        }
+
+        public void StartClient()
+        {
+            clientThread = new Thread(HandelClient);
+
+
+            clientThread.Start();
+        }
+        
+        public void HandelClient()
+        {
+            string text;
+            while (true)
+            {
+                text = sR.ReadLine();
+                Console.WriteLine(text);
+                Broadcast.BroadcastToClients(text);
+            }
+        }
+        public void SendToClient(string messageToClient)
+        {
+            sW.WriteLine(messageToClient);
+            sW.Flush();
+         
+        }
+    }
+}
---
+++ Server til broadcasting sever/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server_til_broadcasting_sever
+{
+    class Program
+    {
+        TcpListener listner = new TcpListener(IPAddress.Any, 11000);
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.Run();
+        }
+        public void Run()
+        {
+            listner.Start();
+            TcpClient tempClient;
+            ClientHandler clienthandler;
+            while (true)
+            {
+                tempClient = listner.AcceptTcpClient();
+                Console.WriteLine("Here you are");
+                clienthandler = new ClientHandler(tempClient);
+                Broadcast.addClientToList(clienthandler);
+            }
+        }
+    }
+}
